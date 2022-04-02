1222f7934925e1643f5a4bf4e40326d545cd4c94
rohannaido <87189343+rohannaido@users.noreply.github.com>
2022-04-02 10:28:43 +0530

    style: blur background and nav bar hide

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const imageContainers = document.querySelectorAll('.image-container');
 const displayImageBox = document.querySelector('.display-image-background');
 const closeImageButton = document.querySelector('.close');
+const mainContainer = document.querySelector('.main-container');
+const navBar = document.querySelector('.nav-bar-div');
 
 const imageDisplay = (imageContainer) => {
     imageContainer.addEventListener('click',() => {
             displayImageBox.style.display = 'block';
             document.querySelector('.display-one-image').src = imageContainer.querySelector('img').src;
+            mainContainer.style.filter = 'blur(8px)';
+            navBar.style.display = 'none';
         })
 }
 
@@ -13,8 +17,7 @@ imageContainers.forEach(imageDisplay);
 
 closeImageButton.addEventListener('click', () => {
     displayImageBox.style.display = 'none';
+    mainContainer.style.filter = 'none';
+    navBar.style.display = 'flex';
 })
 
-// const headerNav = document.querySelector('.header-nav');
-// // const rect = headerNav.getBoundingClientRect();
-// // console.log(rect.top, rect.right, rect.bottom, rect.left);
